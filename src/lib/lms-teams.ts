b39b9c5c19a0544a478b8a0e785c56dcc9b1749e
donabatelodge-wq <donabatelodge@gmail.com>
2026-09-21 17:40:13 +0100

    Create lms-teams.ts

---
+++ src/lib/lms-teams.ts
@@ -0,0 +1,6 @@
+export const PL_TEAMS = [
+  "Arsenal","Aston Villa","Bournemouth","Brentford","Brighton","Chelsea",
+  "Crystal Palace","Everton","Fulham","Ipswich Town","Leicester City","Liverpool",
+  "Manchester City","Manchester United","Newcastle United","Nottingham Forest",
+  "Southampton","Tottenham Hotspur","West Ham United","Wolverhampton Wanderers"
+];
